147e9e1389fa260711fa6a334c65834a8a8c4193
Pauline Davis <pauljdavi.t21@btinternet.com>
2023-06-05 11:22:59 +0100

    added linked list collection exercises

--- exercises/java-collections/src/main/java/com/cbfacademy/CollectionsExercises.java
+++ exercises/java-collections/src/main/java/com/cbfacademy/CollectionsExercises.java
@@ -1,21 +1,40 @@
 package com.cbfacademy;
 
-import java.util.*;
+import java.util.LinkedList;
 
 public class CollectionsExercises {
 
     public LinkedList<Integer> useLinkedList() {
         // TODO: create an empty linked list
+        LinkedList<Integer> emptyLinkedList = new LinkedList<>();
         //  - add 4 as the first element of the list
-        //  - then add 5, 6, 8, 2, 9 to the the list
+        emptyLinkedList.addFirst(4);
+        //  - then add 5, 6, 8, 2, 9 to the list
+        emptyLinkedList.add(5);
+        emptyLinkedList.add(6);
+        emptyLinkedList.add(8);
+        emptyLinkedList.add(2);
+        emptyLinkedList.add(9);
         //  - add another 2 as the last element of the list
+        emptyLinkedList.addLast(2);
         //  - add 4 as the 3rd element of the list
+        emptyLinkedList.add(2, 4);
         //  - invoke the method element() on the list and print the result on the screen
+        int firstElement = emptyLinkedList.getFirst();
+        System.out.println("First Element: " + firstElement);
         //  - return the list
-        throw new RuntimeException("Not implemented");
+        System.out.println("Empty Linked List: " + emptyLinkedList);
+        return emptyLinkedList;
     }
 
-    public Stack<Integer> useStack() {
+    public static void main(String[] args) {
+        CollectionsExercises collectionsExercises = new CollectionsExercises();
+        collectionsExercises.useLinkedList();
+    }
+}
+
+
+//    public Stack<Integer> useStack() {
         // TODO: create an empty stack
         //  - add 5, 6, 8, 9 to the the stack
         //  - print the first element of the stack on the screen
@@ -23,36 +42,36 @@ public class CollectionsExercises {
         //  - invoke the method pop() on the stack and print the result on the screen
         //  - invoke the push(4) method on the stack
         //  - return the stack
-        throw new RuntimeException("Not implemented");
-    }
+        //throw new RuntimeException("Not implemented");
+    //}
+//
+//    public ArrayDeque<Integer> useArrayDeque() {
+//        // TODO: create an empty arrayDeque
+//        //  - add 5, 6, 8, 9 to the the stack
+//        //  - print the first element of the queue on the screen
+//        //  - print the last element of the queue on the screen
+//        //  - invoke the method poll() on the queue and print the result on the screen
+//        //  - invoke the element() method on the queue and print the result on the screen
+//        //  - return the queue
+//        throw new RuntimeException("Not implemented");
+//    }
+//
+//    public HashMap<Integer, String> useHashMap() {
+//        // TODO: create an empty hash map
+//        //  - add {1, TypeScript} entry to the map
+//        //  - add {2, Kotlin} entry to the map
+//        //  - add {3, Python} entry to the map
+//        //  - add {4, Java} entry to the map
+//        //  - add {5, JavaScript} entry to the map
+//        //  - add {6, Rust} entry to the map
+//        //  - determine the set of keys from the map and print it on the screen
+//        //  - determine the set of keys from the map and print it on the screen
+//        //  - determine whether the map contains "English" as a language and print the result on the screen
+//        //  - return the map
+//        throw new RuntimeException("Not implemented");
+//    }
 
-    public ArrayDeque<Integer> useArrayDeque() {
-        // TODO: create an empty arrayDeque
-        //  - add 5, 6, 8, 9 to the the stack
-        //  - print the first element of the queue on the screen
-        //  - print the last element of the queue on the screen
-        //  - invoke the method poll() on the queue and print the result on the screen
-        //  - invoke the element() method on the queue and print the result on the screen
-        //  - return the queue
-        throw new RuntimeException("Not implemented");
-    }
-
-    public HashMap<Integer, String> useHashMap() {
-        // TODO: create an empty hash map
-        //  - add {1, TypeScript} entry to the map
-        //  - add {2, Kotlin} entry to the map
-        //  - add {3, Python} entry to the map
-        //  - add {4, Java} entry to the map
-        //  - add {5, JavaScript} entry to the map
-        //  - add {6, Rust} entry to the map
-        //  - determine the set of keys from the map and print it on the screen
-        //  - determine the set of keys from the map and print it on the screen
-        //  - determine whether the map contains "English" as a language and print the result on the screen
-        //  - return the map
-        throw new RuntimeException("Not implemented");
-    }
-
-    public String getName() {
-        return "Collections Exercises";
-    }
-}
+//    public String getName() {
+//        return "Collections Exercises";
+//    }
+//}
